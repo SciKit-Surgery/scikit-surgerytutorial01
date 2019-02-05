ca28c46de95ca7d26c2247251ea641b3ab64d105
Stephen Thompson <s.thompson@ucl.ac.uk>
2019-02-05 16:27:01 +0000

    Issue #5 tidied up scripts so the pass linting

--- snappytutorial01/01_vtkoverlay_with_movement_app.py
+++ snappytutorial01/01_vtkoverlay_with_movement_app.py
@@ -1,5 +1,8 @@
-#! /usr/bin/python3.6
 # coding=utf-8
+
+"""Script to create a viewer window with a moving surface
+model overlaid in a live video feed"""
+
 import sys
 from PySide2.QtWidgets import QApplication
 from sksurgeryvtk.widgets.OverlayBaseApp import OverlayBaseApp
@@ -11,30 +14,31 @@ class OverlayApp(OverlayBaseApp):
     def update(self):
         """Update the background renderer with a new frame,
         move the model and render"""
-        ret, self.img = self.video_source.read()
+        _, image = self.video_source.read()
 
         #add a method to move the rendered models
         self._move_model()
 
-        self.vtk_overlay_window.set_video_image(self.img)
-        self.vtk_overlay_window._RenderWindow.Render()
+        self.vtk_overlay_window.set_video_image(image)
+        self.vtk_overlay_window.Render()
 
     def _move_model(self):
         """Internal method to move the rendered models in
         some interesting way"""
         #Iterate through the rendered models
-        for actor in self.vtk_overlay_window.get_foreground_renderer().GetActors():
+        for actor in \
+                self.vtk_overlay_window.get_foreground_renderer().GetActors():
             #get the current orientation
-            orientation=actor.GetOrientation()
+            orientation = actor.GetOrientation()
             #increase the rotation around the z-axis by 1.0 degrees
-            orientation = [orientation[0] , orientation[1], orientation[2] + 1.0]
+            orientation = [orientation[0], orientation[1], orientation[2] + 1.0]
             #add update the model's orientation
             actor.SetOrientation(orientation)
 
 
 app = QApplication([])
 
-video_source=0
+video_source = 0
 viewer = OverlayApp(video_source)
 
 model_dir = '../models'
@@ -43,4 +47,3 @@ viewer.add_vtk_models_from_dir(model_dir)
 viewer.start()
 
 sys.exit(app.exec_())
-
--- snappytutorial01/02_vtk_aruco_app.py
+++ snappytutorial01/02_vtk_aruco_app.py
@@ -1,8 +1,11 @@
-#! /usr/bin/python3.6
 # coding=utf-8
+
+"""Script to create a viewer window with a movable surface
+model overlaid in a live video feed"""
+
 import sys
 #add an import for opencv, so we can use its aruco functions
-import cv2
+import cv2.aruco as aruco
 #add an import for numpy, to manipulate arrays
 import numpy
 from PySide2.QtWidgets import QApplication
@@ -12,56 +15,59 @@ class OverlayApp(OverlayBaseApp):
     """Inherits from OverlayBaseApp, and adds methods to
     detect aruco tags and move the model to follow."""
 
-    def __init__(self, video_source):
+    def __init__(self, image_source):
         """overrides the default constructor to add some member variables
         which wee need for the aruco tag detection"""
 
         #the aruco tag dictionary to use. DICT_4X4_50 will work with the tag in
         #../tags/aruco_4by4_0.pdf
-        self.dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+        self.dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
         # The size of the aruco tag in mm
         self.marker_size = 50
 
         #we'll use opencv to estimate the pose of the visible aruco tags.
         #for that we need a calibrated camera. For now let's just use a
         #a hard coded estimate. Maybe you could improve on this.
-        self.camera_projection_matrix = numpy.array([[560.0 , 0.0 , 320.0],
-                                                      [0.0, 560.0 , 240.0],
-                                                      [0.0, 0.0, 1.0]])
-        self.camera_distortion = numpy.zeros((1,4),numpy.float32)
+        self.camera_projection_mat = numpy.array([[560.0, 0.0, 320.0],
+                                                  [0.0, 560.0, 240.0],
+                                                  [0.0, 0.0, 1.0]])
+        self.camera_distortion = numpy.zeros((1, 4), numpy.float32)
 
         #and call the constructor for the base class
-        if (sys.version_info > (3, 0)):
-            super().__init__(video_source)
+        if sys.version_info > (3, 0):
+            super().__init__(image_source)
         else:
             #super doesn't work the same in py2.7
-            OverlayBaseApp.__init__(self,video_source)
+            OverlayBaseApp.__init__(self, image_source)
 
     def update(self):
         """Update the background render with a new frame and
         scan for aruco tags"""
 
-        ret, self.img = self.video_source.read()
-        self._aruco_detect_and_follow()
+        _, image = self.video_source.read()
+        self._aruco_detect_and_follow(image)
 
         #Without the next line the model does not show as the clipping range
         #does not change to accommodate model motion. Uncomment it to
         #see what happens.
-        self.vtk_overlay_window.set_camera_state({"ClippingRange": [10,800]})
-        self.vtk_overlay_window.set_video_image(self.img)
-        self.vtk_overlay_window._RenderWindow.Render()
+        self.vtk_overlay_window.set_camera_state({"ClippingRange": [10, 800]})
+        self.vtk_overlay_window.set_video_image(image)
+        self.vtk_overlay_window.Render()
 
-    def _aruco_detect_and_follow(self):
+    def _aruco_detect_and_follow(self, image):
         """Detect any aruco tags present. Based on;
         https://docs.opencv.org/3.4/d5/dae/tutorial_aruco_detection.html
         """
         #detect any markers
-        marker_corners, marker_ids, _ = cv2.aruco.detectMarkers(self.img, self.dictionary)
+        marker_corners, _, _ = aruco.detectMarkers(image, self.dictionary)
 
-        if len(marker_corners) > 0:
+        if marker_corners:
             #if any markers found, try and determine their pose
-            rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(marker_corners,
-                    self.marker_size, self.camera_projection_matrix, self.camera_distortion)
+            rvecs, tvecs, _ = \
+                    aruco.estimatePoseSingleMarkers(marker_corners,
+                                                    self.marker_size,
+                                                    self.camera_projection_mat,
+                                                    self.camera_distortion)
 
             self._move_model(rvecs[0][0], tvecs[0][0])
 
@@ -72,26 +78,27 @@ class OverlayApp(OverlayBaseApp):
 
         #because the camera won't normally be at the origin,
         #we need to find it and make movement relative to it
-        camera=self.vtk_overlay_window.get_foreground_camera()
+        camera = self.vtk_overlay_window.get_foreground_camera()
 
         #Iterate through the rendered models
-        for actor in self.vtk_overlay_window.get_foreground_renderer().GetActors():
+        for actor in \
+                self.vtk_overlay_window.get_foreground_renderer().GetActors():
              #opencv and vtk seem to have different x-axis, flip the x-axis
-             translation[0] = -translation[0]
+            translation[0] = -translation[0]
 
-             #set the position, relative to the camera
-             actor.SetPosition(camera.GetPosition() - translation)
+            #set the position, relative to the camera
+            actor.SetPosition(camera.GetPosition() - translation)
 
-             #for orientation, opencv axes don't line up with VTK, and
-             #rvecs are in radians, VTK in degrees.
-             #uncomment the next two lines for some interesting results.
-             #rotation = 180 * rotation/3.14
-             #actor.SetOrientation( rotation)
+            #rvecs are in radians, VTK in degrees.
+            rotation = 180 * rotation/3.14
 
+            #for orientation, opencv axes don't line up with VTK,
+            #uncomment the next line for some interesting results.
+            #actor.SetOrientation( rotation)
 
 app = QApplication([])
 
-video_source=0
+video_source = 0
 viewer = OverlayApp(video_source)
 
 model_dir = '../models'
@@ -100,4 +107,3 @@ viewer.add_vtk_models_from_dir(model_dir)
 viewer.start()
 
 sys.exit(app.exec_())
-
