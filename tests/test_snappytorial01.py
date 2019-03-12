38c4a291bb31340170af3dce5eef47df3ae5ed15
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2019-03-12 13:02:48 +0000

    Issue #5 changed handling of qapplication in tests, based on surgeryutils

--- tests/test_snappytorial01.py
+++ tests/test_snappytorial01.py
@@ -2,22 +2,22 @@
   
 """snappytutorial01 vtkoverlay tests"""
 
+import pytest
+
 from snappytutorial01.vtk_aruco_app import OverlayApp as aruco
 from snappytutorial01.vtkoverlay_app import OverlayApp as vtkoverlay
 from snappytutorial01.vtkoverlay_with_movement_app import OverlayApp as vtkmovingoverlay
-from PySide2.QtWidgets import QApplication
 
-def test_vtk_aruco_app():
-    app = QApplication([])
+def test_vtk_aruco_app(setup_qt):
     viewer = aruco(image_source = 0)
     assert True
 
 
-def test_vtkoverlay_app():
+def test_vtkoverlay_app(setup_qt):
     viewer = vtkoverlay(video_source = 0)
     assert True
 
-def test_vtkoverlay_with_movement_app():
+def test_vtkoverlay_with_movement_app(setup_qt):
     viewer = vtkmovingoverlay(video_source = 0)
     assert True
 
