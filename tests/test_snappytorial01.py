b977125d07aebbc41f3534c75aa0e234c63fbc7a
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2019-03-12 13:58:40 +0000

    Issue #5 put tests back in, and put back py27

--- tests/test_snappytorial01.py
+++ tests/test_snappytorial01.py
@@ -8,18 +8,15 @@ from snappytutorial01.vtk_aruco_app import OverlayApp as aruco
 from snappytutorial01.vtkoverlay_app import OverlayApp as vtkoverlay
 from snappytutorial01.vtkoverlay_with_movement_app import OverlayApp as vtkmovingoverlay
 
-def test():
+def test_vtk_aruco_app(setup_qt):
+    viewer = aruco(image_source = 0)
     assert True
-#def test_vtk_aruco_app(setup_qt):
-#    viewer = aruco(image_source = 0)
-#    assert True
-#
-#
-#def test_vtkoverlay_app(setup_qt):
-#    viewer = vtkoverlay(video_source = 0)
-#    assert True
 
-#def test_vtkoverlay_with_movement_app(setup_qt):
-#    viewer = vtkmovingoverlay(video_source = 0)
-#    assert True
+def test_vtkoverlay_app(setup_qt):
+    viewer = vtkoverlay(video_source = 0)
+    assert True
+
+def test_vtkoverlay_with_movement_app(setup_qt):
+    viewer = vtkmovingoverlay(video_source = 0)
+    assert True
 
