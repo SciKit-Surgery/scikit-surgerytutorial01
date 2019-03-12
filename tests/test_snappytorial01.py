ba1b6eecd175a67ac46f0c5a7f7b7ae22a74a32e
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2019-03-12 16:02:37 +0000

    Issue #5 more testing

--- tests/test_snappytorial01.py
+++ tests/test_snappytorial01.py
@@ -10,13 +10,19 @@ from snappytutorial01.vtkoverlay_with_movement_app import OverlayApp as vtkmovin
 
 def test_vtk_aruco_app(setup_qt):
     viewer = aruco(image_source = 'data/output.avi')
+    viewer.add_vtk_models_from_dir('models')
+    viewer.start()
     assert True
 
 def test_vtkoverlay_app(setup_qt):
     viewer = vtkoverlay(video_source = 'data/output.avi')
+    viewer.add_vtk_models_from_dir('models')
+    viewer.start()
     assert True
 
 def test_vtkoverlay_with_movement_app(setup_qt):
     viewer = vtkmovingoverlay(video_source = 'data/output.avi')
+    viewer.add_vtk_models_from_dir('models')
+    viewer.start()
     assert True
 
