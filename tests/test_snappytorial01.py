141ed8949ed4b1dbcffba703e1e145d5ab919aa5
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2019-03-14 16:19:21 +0000

    Issue #5 put tests back in

--- tests/test_snappytorial01.py
+++ tests/test_snappytorial01.py
@@ -8,30 +8,25 @@ from snappytutorial01.vtk_aruco_app import OverlayApp as aruco
 from snappytutorial01.vtkoverlay_app import OverlayApp as vtkoverlay
 from snappytutorial01.vtkoverlay_with_movement_app import OverlayApp as vtkmovingoverlay
 
-
-#I can't seem to get the following to run on all platforms, I suspect it's
-#something to do with display, but I need to park it for the minute, so 
-#I've just commented out the lines that start the viewer
-
 def test_vtk_aruco_app(setup_qt):
     pass
-#    viewer = aruco(image_source = 'data/output.avi')
-#    viewer.add_vtk_models_from_dir('models')
-#    viewer.start()
-#    assert True
+    viewer = aruco(image_source = 'data/output.avi')
+    viewer.add_vtk_models_from_dir('models')
+    viewer.start()
+    viewer.stop()
 
 def test_vtkoverlay_app(setup_qt):
     pass
-#    viewer = vtkoverlay(video_source = 'data/output.avi')
-#    viewer.add_vtk_models_from_dir('models')
-#    viewer.start()
-#    assert True
+    viewer = vtkoverlay(video_source = 'data/output.avi')
+    viewer.add_vtk_models_from_dir('models')
+    viewer.start()
+    viewer.stop()
 
 def test_vtkoverlay_with_movement_app(setup_qt):
     pass
-#    viewer = vtkmovingoverlay(video_source = 'data/output.avi')
-#    viewer.add_vtk_models_from_dir('models')
-#    viewer.start()
-#    assert True
+    viewer = vtkmovingoverlay(video_source = 'data/output.avi')
+    viewer.add_vtk_models_from_dir('models')
+    viewer.start()
+    viewer.stop()
 
 
