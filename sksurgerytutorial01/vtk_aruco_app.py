0638903dfcfc720301babab56c713c813c222a88
Stephen Thompson <s.thompson@ucl.ac.uk>
2020-08-04 11:00:53 +0100

    Issue #16 kind of working, transforms not quite right

--- sksurgerytutorial01/vtk_aruco_app.py
+++ sksurgerytutorial01/vtk_aruco_app.py
@@ -9,7 +9,10 @@ import cv2.aruco as aruco
 #add an import for numpy, to manipulate arrays
 import numpy
 from PySide2.QtWidgets import QApplication
+from sksurgerycore.transforms.transform_manager import TransformManager
 from sksurgeryutils.common_overlay_apps import OverlayBaseApp
+from sksurgeryvtk.utils.matrix_utils import create_vtk_matrix_from_numpy
+from sksurgeryarucotracker.arucotracker import ArUcoTracker
 
 class OverlayApp(OverlayBaseApp):
     """Inherits from OverlayBaseApp, and adds methods to
@@ -21,17 +24,24 @@ class OverlayApp(OverlayBaseApp):
 
         #the aruco tag dictionary to use. DICT_4X4_50 will work with the tag in
         #../tags/aruco_4by4_0.pdf
-        self.dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-        # The size of the aruco tag in mm
-        self.marker_size = 50
 
         #we'll use opencv to estimate the pose of the visible aruco tags.
         #for that we need a calibrated camera. For now let's just use a
         #a hard coded estimate. Maybe you could improve on this.
-        self.camera_projection_mat = numpy.array([[560.0, 0.0, 320.0],
+
+        ar_config={
+                "tracker type": "aruco",
+                "video source": 'none',
+                "debug": False,
+                "dictionary" : 'DICT_4X4_50',
+                "marker size": 50,
+                "camera projection": numpy.array([[560.0, 0.0, 320.0],
                                                   [0.0, 560.0, 240.0],
-                                                  [0.0, 0.0, 1.0]])
-        self.camera_distortion = numpy.zeros((1, 4), numpy.float32)
+                                                  [0.0, 0.0, 1.0]],
+                                                  dtype=numpy.float32),
+                "camera distortion": numpy.zeros((1, 4), numpy.float32)
+
+                  }
 
         #and call the constructor for the base class
         if sys.version_info > (3, 0):
@@ -39,11 +49,13 @@ class OverlayApp(OverlayBaseApp):
         else:
             #super doesn't work the same in py2.7
             OverlayBaseApp.__init__(self, image_source)
+        
+        self.tracker = ArUcoTracker(ar_config)
+        self.tracker.start_tracking()
 
     def update(self):
         """Update the background render with a new frame and
         scan for aruco tags"""
-
         _, image = self.video_source.read()
         self._aruco_detect_and_follow(image)
 
@@ -55,42 +67,40 @@ class OverlayApp(OverlayBaseApp):
         self.vtk_overlay_window.Render()
 
     def _aruco_detect_and_follow(self, image):
-        """Detect any aruco tags present. Based on;
-        https://docs.opencv.org/3.4/d5/dae/tutorial_aruco_detection.html
+        """Detect any aruco tags present using sksurgeryarucotracker
         """
-        #detect any markers
-        marker_corners, _, _ = aruco.detectMarkers(image, self.dictionary)
-
-        if marker_corners:
-            #if any markers found, try and determine their pose
-            rvecs, tvecs, _ = \
-                    aruco.estimatePoseSingleMarkers(marker_corners,
-                                                    self.marker_size,
-                                                    self.camera_projection_mat,
-                                                    self.camera_distortion)
-
-            self._move_model(rvecs[0][0], tvecs[0][0])
+        
+        port_handles, _, _, tag2camera, _ = self.tracker.get_frame(image)
+        if tag2camera is not None:
+            self._move_model(tag2camera[0])
 
 
-    def _move_model(self, rotation, translation):
+    def _move_model(self, tag2camera):
         """Internal method to move the rendered models in
         some interesting way"""
 
         #because the camera won't normally be at the origin,
         #we need to find it and make movement relative to it
-        camera = self.vtk_overlay_window.get_foreground_camera()
-
+        transform_manager = TransformManager()
+        camera_vtk = self.vtk_overlay_window.get_foreground_camera().GetModelViewTransformMatrix()
+       
+        camera2world = numpy.eye(4, dtype = numpy.float64)
+        camera_vtk.DeepCopy(camera2world.ravel(), camera_vtk)
+        transform_manager.add("camera2world", camera2world)
+        transform_manager.add("camera2tag", tag2camera)
+        tag2world = transform_manager.get("tag2world")
         #Iterate through the rendered models
         for actor in \
                 self.vtk_overlay_window.get_foreground_renderer().GetActors():
             #opencv and vtk seem to have different x-axis, flip the x-axis
-            translation[0] = -translation[0]
+            actor.SetUserMatrix(create_vtk_matrix_from_numpy(tag2world))
+            #translation[0] = -translation[0]
 
             #set the position, relative to the camera
-            actor.SetPosition(camera.GetPosition() - translation)
+            #actor.SetPosition(camera.GetPosition() - translation)
 
             #rvecs are in radians, VTK in degrees.
-            rotation = 180 * rotation/3.14
+            #rotation = 180 * rotation/3.14
 
             #for orientation, opencv axes don't line up with VTK,
             #uncomment the next line for some interesting results.
